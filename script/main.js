0e582f0b92613c0082461314dd308bfac1b4d810
Bryan <bryan@omicron.(none)>
2013-04-21 23:37:38 +0200

    buncha fixes

--- script/main.js
+++ script/main.js
@@ -1,6 +1,38 @@
 $(document).ready(function() {
 
-	$('.slide').css('height',  $(window).height() );
+	$('.intro').css('height',$(window).height());
+
+	$('a#intro').addClass('current');
+
+	$('.nav a').click(function() {
+		$('.nav a').removeClass('current');
+		$(this).addClass('current');
+	});
+
+	$('#intro').click(function() {
+		$.scrollTo('.intro',500);	
+	});
+	$('#about').click(function() {
+		$.scrollTo('.about',500);	
+	});
+	$('#work').click(function() {
+		$.scrollTo('.work',500);	
+	});
+	$('#contact').click(function() {
+		$.scrollTo('.contact',500);	
+	});
+
+	$('.work').waypoint(function() {
+		/*$('.nav a').removeClass('current');
+		$('.nav #intro').addClass('current');*/
+		console.log('work top');
+	});
+	/*
+	$('.about').waypoint(function() {
+		$('.nav a').removeClass('current');
+		$('.nav #about').addClass('current');
+		console.log('about top');
+	});*/
 
 });
 
@@ -8,4 +40,8 @@ $(window).resize(function() {
 
 	$('.slide').css('height', $(window).height());
 	
+	if( $('.intro').attr(position.top) = 0 ) {
+		console.log('top');
+	}
+
 });
