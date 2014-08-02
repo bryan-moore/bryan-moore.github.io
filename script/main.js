f6031cebeff2a87748abcc9b95e7dfc97cc16a5c
Bryan Moore <bryan.moore64@gmail.com>
2014-08-02 09:56:29 +0200

    vib fix

--- script/main.js
+++ script/main.js
@@ -3,15 +3,11 @@ $(document).ready(function() {
     // enable vibration support
     navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
 
-
-
     $('.theme-change').on('click', function() {
         if (navigator.vibrate) {
-            navigator.vibrate(500);
+            navigator.vibrate(100);
         }
         $('html').toggleClass('dark');
     });
-
-
 
 });
